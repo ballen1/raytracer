613e0279dec11fd50b7625bf67389b84b929c732
Brandon Allen <brandonreallen@gmail.com>
2016-11-20 11:43:24 -0500

    Setup framework for ray tracing

--- source.cpp
+++ source.cpp
@@ -4,12 +4,10 @@
   #include <GL/glut.h>
 #endif
 
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 600
+#define WIDTH 600
+#define HEIGHT 600
 #define WINDOW_STARTX 100
 #define WINDOW_STARTY 100
-
-#define EXTENT 50
 
 struct Vec3 
 {
@@ -31,13 +29,29 @@ struct RectanglePlane
     Vec3 points[4];
 };
 
+struct Background
+{
+    float ambientColor[3];
+    float backgroundColor[4];
+};
+
+struct Camera
+{
+    float eye[3];
+    float lookAt[3];
+    float up[3];
+};
+
 static Sphere spheres[3];
+static Background bg;
+static Camera cam;
 
 void glInit();
 void reshape(int width, int height);
 void display();
 void setupMVPMatrices();
 void defineSpheres();
+void traceRay();
 
 int main(int argc, char* argv[])
 {
@@ -52,7 +66,7 @@ int main(int argc, char* argv[])
 void glInit()
 {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    glutInitWindowSize(WIDTH, HEIGHT);
     glutInitWindowPosition(WINDOW_STARTX, WINDOW_STARTY);
 
     glutCreateWindow("Ray Tracer");
@@ -75,7 +89,7 @@ void setupMVPMatrices()
 {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, EXTENT, 0, EXTENT, -1, 1);
+    glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -87,7 +101,15 @@ void display()
     
     glPushMatrix();
     
+    for (int i = 0; i < HEIGHT; i++)
+    {
+	for (int j = 0; j < WIDTH; j++)
+	{
+	    traceRay();
+	}
+    }
     
+
     glPopMatrix();
 
     glutSwapBuffers();
@@ -95,7 +117,56 @@ void display()
     glutPostRedisplay();
 }
 
-void defineSpheres()
+void traceRay() {
+
+}
+
+void defineSceneObjects()
 {
+    
+    // Define overall background properties
+    bg.backgroundColor[0] = 0.0f;
+    bg.backgroundColor[1] = 0.0f;
+    bg.backgroundColor[2] = 0.0f;
+    bg.backgroundColor[3] = 1.0f;
+
+    bg.ambientColor[0] = 1.0f;
+    bg.ambientColor[1] = 1.0f;
+    bg.ambientColor[2] = 1.0f;
+
+    // Define the spheres
+
+    spheres[0].center.x = 10.0f;
+    spheres[0].center.y = 10.0f;
+    spheres[0].center.z = 10.0f;
+    spheres[0].radius = 5.0f;
+    spheres[0].diffuseColor[0] = 1.0f;
+    spheres[0].diffuseColor[1] = 0.0f;
+    spheres[0].diffuseColor[2] = 0.0f;
+    //spheres[0].specularColor[0] = ;
+    //spheres[0].specularColor[1] = ;
+    //spheres[0].specularColor[2] = ;
+
+    spheres[1].center.x = -10.0f;
+    spheres[1].center.y = 10.0f;
+    spheres[1].center.z = -10.0f;
+    spheres[1].radius = 5.0f;
+    spheres[1].diffuseColor[0] = 0.0f;
+    spheres[1].diffuseColor[1] = 1.0f;
+    spheres[1].diffuseColor[2] = 0.0f;
+    //spheres[1].specularColor[0] = ;
+    //spheres[1].specularColor[1] = ;
+    //spheres[1].specularColor[2] = ;
+
+    spheres[2].center.x = 0.0f;
+    spheres[2].center.y = 5.0f;
+    spheres[2].center.z = 20.0f;
+    spheres[2].radius = 5.0f;
+    spheres[2].diffuseColor[0] = 0.0f;
+    spheres[2].diffuseColor[1] = 0.0f;
+    spheres[2].diffuseColor[2] = 1.0f;
+    //spheres[2].specularColor[0] = ;
+    //spheres[2].specularColor[1] = ;
+    //spheres[2].specularColor[2] = ;
 
 }
